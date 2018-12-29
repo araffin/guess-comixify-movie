a67a0c9cdd2a2f46b2ced7c7068182c4c1103a82
Antonin RAFFIN <antonin.raffin@ensta.org>
2018-12-29 23:21:24 +0100

    Add more films

--- generate.py
+++ generate.py
@@ -2,27 +2,44 @@ import json
 
 
 class Movie(object):
-    """docstring for Movie."""
-    def __init__(self, image, names, url=''):
+    def __init__(self, image, names, id=None):
         super(Movie, self).__init__()
         self.image = image
         if isinstance(names, str):
             names = [names]
         self.names = names
-        self.url = url
+        self.id = id
+
+# https://www.themoviedb.org/movie/id-name
 
 movies = [
             [
                 'img/0.jpg', 'Apocalypse Now',
-                'https://www.themoviedb.org/movie/28-apocalypse-now'
+                28
             ],
             [
                 'img/1.jpg', ['La Guerre des étoiles', 'Star Wars'],
-                'https://www.themoviedb.org/movie/11-star-wars'
+                11
             ],
             [
-                'img/2.png', 'Les Visiteurs',
-                'https://www.themoviedb.org/movie/11687-les-visiteurs'
+                'img/2.png', ['Les Visiteurs', 'The Visitors'],
+                11687
+            ],
+            [
+                'img/3.png', ['Le Cercle des poètes disparus', 'Dead Poets Society'],
+                 207
+            ],
+            [
+                'img/4.png', ['Les tontons flingueurs', 'Crooks in Clover'],
+                 25253
+            ],
+            [
+                'img/5.png', 'Deadpool',
+                 293660
+            ],
+            [
+                'img/6.png', 'Baby Driver',
+                 339403
             ],
         ]
 
